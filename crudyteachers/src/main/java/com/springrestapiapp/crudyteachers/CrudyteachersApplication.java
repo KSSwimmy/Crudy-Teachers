c165df53b0da19d6a89c16b177cbb8399782ef47
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-03-05 23:15:28 -0500

    :ok_hand: added function packages, we're ready to code!
    added function packages, we're ready to code!

---
+++ crudyteachers/src/main/java/com/springrestapiapp/crudyteachers/CrudyteachersApplication.java
@@ -0,0 +1,17 @@
+package com.springrestapiapp.crudyteachers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //YOU HAVE A SPRING BOOT APP NOW!
+                            // CONGRATS! FROM ME: KIMBERLY :D
+
+public class CrudyteachersApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CrudyteachersApplication.class, args);
+    }
+
+}
---
+++ crudyteachers/src/test/java/com/springrestapiapp/crudyteachers/CrudyteachersApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springrestapiapp.crudyteachers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudyteachersApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
