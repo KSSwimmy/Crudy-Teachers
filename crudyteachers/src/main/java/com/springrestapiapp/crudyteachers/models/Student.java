891c032d0ffb2a184b0f1c11093ac613e5465cee
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-03-06 22:57:16 -0500

    :pencil: Student Model is done
    Student Model is done

--- crudyteachers/src/main/java/com/springrestapiapp/crudyteachers/models/Student.java
+++ crudyteachers/src/main/java/com/springrestapiapp/crudyteachers/models/Student.java
@@ -9,7 +9,7 @@ public class Student
 {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long menuid;
+    private long studentid;
 
     @Column(nullable = false)
     private String name;
@@ -20,4 +20,59 @@ public class Student
                                     //JoinColumn connects them.
                 nullable = false)
     private Teacher teacher;
+
+    public Student() // Every time we make a class that is going to be an entity we're going to have to
+                    // create a default constructor because Spring Data JPA Requires it
+    {
+    }
+
+    public Student(String name, double grade, Teacher teacher)
+    {
+        this.name = name;
+        this.grade = grade;
+        this.teacher = teacher;
+    }
+
+    public long getStudentid()
+    {
+        return studentid;
+    }
+
+    public void setStudentid(long studentid) // we need the setter for ID because we'll
+                                            // need it for the dreadful seed data (My least favorite part to be honest 😩)
+                                            // we want to have control over the id
+    {
+        this.studentid = studentid;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public double getGrade()
+    {
+        return grade;
+    }
+
+    public void setGrade(double grade)
+    {
+        this.grade = grade;
+    }
+
+    public Teacher getTeacher()
+    {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher)
+    {
+        this.teacher = teacher;
+    }
 }
+
