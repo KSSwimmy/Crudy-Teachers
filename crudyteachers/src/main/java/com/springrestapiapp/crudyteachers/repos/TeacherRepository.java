c2a6de98137d72d7a7772cddaae1e6f9ffb14314
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-03-07 00:52:06 -0500

    :green_apple: Teacher Repository is done for now
    Teacher Repository is done for now

---
+++ crudyteachers/src/main/java/com/springrestapiapp/crudyteachers/repos/TeacherRepository.java
@@ -0,0 +1,12 @@
+package com.springrestapiapp.crudyteachers.repos;
+// This repo is an Interface. Not a Class
+
+
+import com.springrestapiapp.crudyteachers.models.Teacher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeacherRepository extends CrudRepository<Teacher, Long>
+{
+}
+
+// Teacher is the class that CrudRepository takes on as a one of its params and it also takes on the datatype of the Id. Long is the class of the datatype.
