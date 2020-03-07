89e23a271b74ec866b6629a7146c38f9c1f34dfb
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-03-06 23:28:39 -0500

    :apple: Teacher model is donzzies
    Teacher model is donzzies

--- crudyteachers/src/main/java/com/springrestapiapp/crudyteachers/models/Teacher.java
+++ crudyteachers/src/main/java/com/springrestapiapp/crudyteachers/models/Teacher.java
@@ -28,4 +28,88 @@ public class Teacher // singular Classes
                 cascade = CascadeType.ALL) // If you worked with teacher then you'd also be able to work with students.
                                             // This is bridging teacher and student together
     private List<Student> students = new ArrayList<>();
+
+    public Teacher() // If you don't have this default constructor you'll get really weird error messages! TRUST ME YOU'LL NEED THIS!
+    {
+    }
+
+    public Teacher(String name, String address, String city, String state, String telephone)
+    {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.telephone = telephone;
+        // NOTICE: I didn't bring the list in. We just create constructors for the base fields.
+    }
+
+    public long getTeacherid()
+    {
+        return teacherid;
+    }
+
+    public void setTeacherid(long teacherid)
+    {
+        this.teacherid = teacherid;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public String getState()
+    {
+        return state;
+    }
+
+    public void setState(String state)
+    {
+        this.state = state;
+    }
+
+    public String getTelephone()
+    {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone)
+    {
+        this.telephone = telephone;
+    }
+
+    public List<Student> getStudents()
+    {
+        return students;
+    }
+
+    public void setStudents(List<Student> students)
+    {
+        this.students = students;
+    }
 }
