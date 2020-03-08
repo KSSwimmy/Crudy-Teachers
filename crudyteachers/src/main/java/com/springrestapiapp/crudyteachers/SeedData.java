cf6349fc4855ff62af82be6a0df2aa7cb3afe36b
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-03-08 00:14:22 -0500

    :seedling: adding data.SQL disabled SeedData
    adding data.SQL disabled SeedData

--- crudyteachers/src/main/java/com/springrestapiapp/crudyteachers/SeedData.java
+++ crudyteachers/src/main/java/com/springrestapiapp/crudyteachers/SeedData.java
@@ -8,7 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-@Component
+//@Component
 @Transactional // pick the @Transactional that's associated with the Spring Framework
 public class SeedData implements CommandLineRunner
 {
---
+++ crudyteachers/src/main/resources/data.sql
@@ -0,0 +1,22 @@
+DELETE
+FROM student;
+
+DELETE
+FROM teacher;
+
+INSERT INTO teacher (teacherid, name, address, city, state, telephone)
+    VALUES (1, 'Dan L', '123 Main Street', 'City', 'ST', '555-555-1234'),
+           (2, 'Dan S', '321 Uptown Drive', 'Town', 'ST', '555-555-5555'),
+           (3, 'Dustin M', '565 Side Avenue', 'Village', 'ST', '555-123-1555');
+
+INSERT INTO student (studentid, name, grade, teacherid)
+    VALUES (4, 'Doc', 99.95, 1),
+           (5, 'Chad', 89.50, 1),
+           (6, 'Jason', 77.77, 1),
+           (7, 'Jose', 85.49, 1),
+           (8, 'Luis', 89.50, 1),
+           (9, 'Carlos', 99.49, 2),
+           (10, 'Justin', 78.75, 2),
+           (11, 'Kim', 89.89, 3);
+
+alter sequence hibernate_sequence restart with 15;
